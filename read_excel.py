d8e89c621bebff28ddfd1bed683f939269c6a729
S Rosenberg <sbastian.rosenberg@gmail.com>
2023-02-03 20:02:21 -0300

    read_excel.py add parse date

--- read_excel.py
+++ read_excel.py
@@ -1,16 +1,28 @@
-import gspread
+from datetime import datetime
 import pandas as pd
+import gspread
 import os
 
-SHEET_ID = os.getenv('SPREADSHEET_ID')
-SHEET_NAME = os.getenv('WORKSHEET_NAME')
+class ExcelReader:
+    
+    @staticmethod
+    def excel_to_dataframe(sheet_id:str, sheet_name:str) -> pd.DataFrame:
 
-gc = gspread.service_account('credentials.json')
-spreadsheet = gc.open_by_key(SHEET_ID)
-worksheet = spreadsheet.worksheet(SHEET_NAME)
-rows = worksheet.get_all_records()
-print(rows[:5])
+        gc = gspread.service_account('credentials.json')
+        spreadsheet = gc.open_by_key(sheet_id)
+        worksheet = spreadsheet.worksheet(sheet_name)
+        rows = worksheet.get_all_records()
+        df = pd.DataFrame(rows)
 
-print('==============================')
-df = pd.DataFrame(rows)
-print(df.head())
+        return df
+    
+    @staticmethod
+    def parse_date(date: str) -> datetime:
+        day, month, year = date.split('/')
+        return datetime(int(year),int(month), int(day))
+
+if __name__ == '__main__':
+    SHEET_ID = os.getenv('SPREADSHEET_ID')
+    SHEET_NAME = os.getenv('WORKSHEET_NAME')
+
+    data_frame = ExcelReader.excel_to_dataframe(SHEET_ID, SHEET_NAME)
